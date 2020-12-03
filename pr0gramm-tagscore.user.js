3017ca8bb66f3d2f35687b6f9e6a188db0baa3ae
PossumInABox <34094714+PossumInABox@users.noreply.github.com>
2020-12-03 14:00:23 +0100

    Big Update to Version 1.1 Remove jQuery completely Trigger script automatically on change post (without polling)

--- pr0gramm-tagscore.user.js
+++ pr0gramm-tagscore.user.js
@@ -1,71 +1,108 @@
 // ==UserScript==
 // @name         pr0gramm Tagscore
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Userscript for the image board pr0gramm to get scores of posts tags into frontend
-// @author       PossumInAbox
+// @author       PossumInABox
 // @grant        none
 // @include		 https://pr0gramm.com*
+// @exclude      http://full.pr0gramm.com*
+// @exclude		 https://full.pr0gramm.com*
 // @downloadURL  https://possuminabox.github.io/pr0gramm-Tagscore/pr0gramm-tagscore.user.js
 // @updateURL    https://possuminabox.github.io/pr0gramm-Tagscore/pr0gramm-tagscore.user.js
 // ==/UserScript==
 
-var $ = window.jQuery;
+(function tagScoreUserScript() {
+    console.debug("Loaded module: pr0gramm Tagscore")
 
-function getPostId() {
-    let urlString = window.location.href;
-    let urlList0 = urlString.split('/');
-    let urlList1 = (urlList0[urlList0.length - 1]).split(':');
-    return urlList1[0];
-}
+    function getPostId() {
+        let urlString = window.location.href;
+        let urlList0 = urlString.split('/');
+        let urlList1 = (urlList0[urlList0.length - 1]).split(':');
+        let postNumber = urlList1[0]
 
-(function() {
+        if (postNumber.match("[0-9]+")) {
+            return postNumber
+        } else {
+            return false
+        }
+    }
 
-    document.onreadystatechange = function() {
 
+    async function initTagScore() {
 
         let apiURL = ('https://pr0gramm.com/api/items/info?itemId=' + getPostId());
+        if (!apiURL) {return;}
+
+        let data = await fetch(apiURL, {method: 'GET', 'headers': {}})
+        data = await data.json();
+
+        let tags = data.tags;
+
+        let pageTags = document.querySelectorAll('.tags .tag');
+
+        for (let k = 0; k < pageTags.length; k++) {
+
+            let currentItem = pageTags[k];
+            let nodeId = currentItem.id;
 
 
-        $.get(apiURL, function(data) {
-            let general0 = data;
-            //console.log(data);
-            //var general = JSON.parse(data);
-            let tags = data.tags;
-            //console.log(tags);
-
-            let pageTags = $('.tags .tag');
-
-            for (var k = 0; k < pageTags.length; k++) {
-
-                let currentItem = pageTags[k];
-                let nodeId = currentItem.id;
-
-                for (let i = 0; i < tags.length; i++) {
-                    let currentTagMatch = 'tag-' + tags[i].id;
-                    if (currentTagMatch == nodeId) {
-                        let roundedConfidence = (tags[i].confidence * 100).toFixed(3);
-                        let tagClass = 'baseValue';
-                        if (roundedConfidence > 20.654) {
-                            tagClass = 'upvotedTag';
-                        } else if (roundedConfidence < 20.654) {
-                            tagClass = 'downvotedTag'
-                        }
-                        let confidenceString = (' | <span class="' + tagClass + '">' + roundedConfidence + ' %</span>');
-                        $(('#'+nodeId+' a')).append(confidenceString);
-
-                        // setting css
-                        $(".baseValue").css('color', 'yellow');
-                        $(".upvotedTag").css('color', 'green');
-                        $(".downvotedTag").css('color', 'red');
-
+            for (let i = 0; i < tags.length; i++) {
+                let currentTagMatch = 'tag-' + tags[i].id;
+                if (currentTagMatch == nodeId) {
+                    let roundedConfidence = (tags[i].confidence * 100).toFixed(3);
+                    let tagClass = 'baseValue';
+                    if (roundedConfidence > 20.654) {
+                        tagClass = 'upvotedTag';
+                    } else if (roundedConfidence < 20.654) {
+                        tagClass = 'downvotedTag'
                     }
+                    let confidenceString = (' | <span class="' + tagClass + '">' + roundedConfidence + ' %</span>');
+                    currentItem.querySelector("a").innerHTML += confidenceString;
+
+                    document.querySelectorAll(".baseValue").forEach(element => {element.style.color = "yellow"})
+                    document.querySelectorAll(".upvotedTag").forEach(element => {element.style.color = "green"})
+                    document.querySelectorAll(".downvotedTag").forEach(element => {element.style.color = "red"})
                 }
             }
+        }
 
-        });
 
     }
+
+
+    // see https://stackoverflow.com/a/46428962/10765287
+    var oldHref = document.location.href;
+
+    window.onload = function() {
+
+        var
+            bodyList = document.querySelector("body")
+
+            ,observer = new MutationObserver(function(mutations) {
+
+                mutations.forEach(function(mutation) {
+
+                    if (oldHref != document.location.href) {
+
+                        oldHref = document.location.href;
+
+                        initTagScore();
+
+                    }
+
+                });
+
+            });
+
+        var config = {
+            childList: true,
+            subtree: true
+        };
+
+        observer.observe(bodyList, config);
+
+    };
 
 
 
